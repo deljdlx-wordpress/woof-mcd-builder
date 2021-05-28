787f727f42d95513dc1ac28b423d063430cd2286
jdlxt <jdlxt@localhost>
2021-05-28 12:48:30 +0200

    helloworld

--- class/Administration.php
+++ class/Administration.php
@@ -4,6 +4,7 @@ namespace WoofSchemaBuilder;
 
 use Woof\Administration as WoofAdministration;
 
+use function Woof\slugify;
 
 class Administration extends WoofAdministration
 {
@@ -11,8 +12,8 @@ class Administration extends WoofAdministration
 
     public function __construct()
     {
-        parent::__construct();
 
+        parent::__construct();
 
         $this->addPage(
             'Woof schema builder',        // label
@@ -27,6 +28,51 @@ class Administration extends WoofAdministration
             'woof-schema-builder'       // parent
         );
     }
+
+    public function addAssets()
+    {
+        parent::addAssets();
+
+        $scripts = [
+            'js/Init.js',
+            'deflate/pako.min.js',
+            'deflate/base64.js',
+            'jscolor/jscolor.js',
+            'sanitizer/sanitizer.min.js',
+            '../assets/mxgraph/src/js/mxClient.js',
+            'js/EditorUi.js',
+            'js/Editor.js',
+            'js/Sidebar.js',
+            'js/Graph.js',
+            'js/Format.js',
+            'js/Shapes.js',
+            'js/Actions.js',
+            'js/Menus.js',
+            'js/Toolbar.js',
+            'js/Dialogs.js',
+            'woof/WoofSchemaEditor.js',
+            'woof/Application.js',
+
+            'woof/bootstrap.js'
+        ];
+        foreach($scripts as $script) {
+            $this->addScript(
+                slugify($script),
+                $this->getPluginURL() . '/public/grapheditor/' . $script,
+                null,
+                null,
+                true
+            );
+        }
+
+
+        $this->addCSS(
+            'grapheditor.css',
+            $this->getPluginURL() . '/public/grapheditor/styles/grapheditor.css'
+        );
+    }
+
+
 
     public function displaySchemaBuilderPage()
     {
